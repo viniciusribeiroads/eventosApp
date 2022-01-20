7ea4b3b9c7803be67e696b06d17f4b9719b9a334
Vinicius Salvador Ribeiro <viniciusribeiro113@gmail.com>
2022-01-20 12:23:01 -0200

    Configurando banco de dados, adicionando dependencias JPA e HIBERNATE

---
+++ src/main/java/com/eventosapp/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.eventosapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.eventoapp.models.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento, String>{
+	
+}
