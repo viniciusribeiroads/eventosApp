fcf36ae1bc8937db07e39f66fa03bc5bce4df2d2
Vinicius Salvador Ribeiro <viniciusribeiro113@gmail.com>
2022-01-23 10:53:15 -0300

    Busca no banco de dados Mysql

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventoapp.models.Evento;
 import com.eventosapp.repository.EventoRepository;
@@ -25,5 +26,13 @@ public class EventoController {
 		er.save(evento);
 		
 		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos); //Passando lista de eventos para a view
+		return mv;
 	}
 }
